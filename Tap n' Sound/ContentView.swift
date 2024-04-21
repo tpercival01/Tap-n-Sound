9e49c03a71ac6ee111792935775a22196dede395
tpercival01 <thomaspercival@live.co.uk>
2024-04-21 16:20:51 +0100

    Initial Commit

---
+++ Tap n' Sound/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tap n' Sound
+//
+//  Created by Thomas Percival on 21/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
