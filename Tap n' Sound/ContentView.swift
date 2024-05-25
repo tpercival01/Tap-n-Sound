2fb2c972fb9f9e31da32eba99ef8a424949af90a
Thomas Percival <thomaspercival@live.co.uk>
2024-05-25 15:20:26 +0100

    Optimized code

--- Tap n' Sound/ContentView.swift
+++ Tap n' Sound/ContentView.swift
@@ -3,11 +3,23 @@ import AVFoundation
 
 struct ContentView: View {
     
-    let images: [UIImage] = [UIImage(named: "A")!, UIImage(named: "B")!, UIImage(named: "C")!, UIImage(named: "D")!, UIImage(named: "E")!, UIImage(named: "F")!, UIImage(named: "G")!, UIImage(named: "H")!, UIImage(named: "I")!, UIImage(named: "J")!, UIImage(named: "K")!, UIImage(named: "L")!, UIImage(named: "M")!, UIImage(named: "N")!, UIImage(named: "O")!, UIImage(named: "P")!, UIImage(named: "Q")!, UIImage(named: "R")!, UIImage(named: "S")!, UIImage(named: "T")!, UIImage(named: "U")!, UIImage(named: "V")!, UIImage(named: "W")!, UIImage(named: "X")!, UIImage(named: "Y")!, UIImage(named: "Z")!, UIImage(named: "0")!, UIImage(named: "1")!, UIImage(named: "2")!, UIImage(named: "3")!, UIImage(named: "4")!, UIImage(named: "5")!, UIImage(named: "6")!, UIImage(named: "7")!, UIImage(named: "8")!, UIImage(named: "9")!]
-        
+    let images: [UIImage] = [
+        UIImage(named: "A")!, UIImage(named: "B")!, UIImage(named: "C")!,
+        UIImage(named: "D")!, UIImage(named: "E")!, UIImage(named: "F")!,
+        UIImage(named: "G")!, UIImage(named: "H")!, UIImage(named: "I")!,
+        UIImage(named: "J")!, UIImage(named: "K")!, UIImage(named: "L")!,
+        UIImage(named: "M")!, UIImage(named: "N")!, UIImage(named: "O")!,
+        UIImage(named: "P")!, UIImage(named: "Q")!, UIImage(named: "R")!,
+        UIImage(named: "S")!, UIImage(named: "T")!, UIImage(named: "U")!,
+        UIImage(named: "V")!, UIImage(named: "W")!, UIImage(named: "X")!,
+        UIImage(named: "Y")!, UIImage(named: "Z")!, UIImage(named: "0")!,
+        UIImage(named: "1")!, UIImage(named: "2")!, UIImage(named: "3")!,
+        UIImage(named: "4")!, UIImage(named: "5")!, UIImage(named: "6")!,
+        UIImage(named: "7")!, UIImage(named: "8")!, UIImage(named: "9")!
+    ]
+    
     @State private var displayedImages: [(image: UIImage, position: CGPoint, size: CGSize)] = []
-    @State private var imageSize: CGSize = CGSize(width: 100, height: 100) // Initial size of the image
-    @StateObject private var audioPlayer = AudioManager()
+    @StateObject private var audioPlayerManager = AudioPlayerManager()
     
     var body: some View {
         ZStack {
@@ -55,15 +67,34 @@ struct ContentView: View {
     }
     
     private func playSound() {
-        audioPlayer.playRandomSound()
+        audioPlayerManager.playRandomSound()
     }
 }
 
-class AudioManager: ObservableObject {
-    var audioPlayers: [AVAudioPlayer] = []
-    let soundFileNames = ["Audio 1_1", "Audio 2_1", "Audio 3_1", "Audio 4_1", "Audio 5_1", "Audio 6_1", "Audio 7_1", "Audio 8_1", "Audio 9_1", "Audio 10_1", "Audio 11_1", "Audio 12_1", "Audio 13_1", "Audio 14_1", "Audio 15_1", "Audio 16_1", "Audio 17_1", "Audio 18_1", "Audio 19_1", "Audio 20_1", "Audio 21_1", "Audio 22_1", "Audio 23_1", "Audio 24_1", "Audio 25_1", "Audio 26_1", "Audio 27_1"]
-
+class AudioPlayerManager: ObservableObject {
+    private var audioPlayers: [AVAudioPlayer] = []
+    private let soundFileNames: [String] = [
+        "Audio 1_1", "Audio 2_1", "Audio 3_1", "Audio 4_1", "Audio 5_1",
+        "Audio 6_1", "Audio 7_1", "Audio 8_1", "Audio 9_1", "Audio 10_1",
+        "Audio 11_1", "Audio 12_1", "Audio 13_1", "Audio 14_1", "Audio 15_1",
+        "Audio 16_1", "Audio 17_1", "Audio 18_1", "Audio 19_1", "Audio 20_1",
+        "Audio 21_1", "Audio 22_1", "Audio 23_1", "Audio 24_1", "Audio 25_1",
+        "Audio 26_1", "Audio 27_1"
+    ]
+    
     init() {
+        // Preload audio players
+        for fileName in soundFileNames {
+            if let soundURL = Bundle.main.url(forResource: fileName, withExtension: "wav") {
+                do {
+                    let audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+                    audioPlayers.append(audioPlayer)
+                } catch {
+                    print("Error loading sound file \(fileName): \(error.localizedDescription)")
+                }
+            }
+        }
+        
         // Configure AVAudioSession to allow mixing
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, options: [.mixWithOthers])
@@ -72,25 +103,15 @@ class AudioManager: ObservableObject {
             print("Failed to set audio session category: \(error.localizedDescription)")
         }
     }
-
+    
     func playRandomSound() {
-        guard let soundFileName = soundFileNames.randomElement() else {
-            print("No sound file found")
+        guard !audioPlayers.isEmpty else {
+            print("No audio players available")
             return
         }
         
-        guard let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav") else {
-            print("Sound file not found")
-            return
-        }
-        
-        do {
-            let audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-            audioPlayers.append(audioPlayer) // Keep a reference to the player to prevent it from being deallocated
-            audioPlayer.play()
-        } catch {
-            print("Error playing sound: \(error.localizedDescription)")
-        }
+        let randomPlayer = audioPlayers.randomElement()!
+        randomPlayer.play()
     }
 }
 
